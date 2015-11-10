6df4d7b2994507666e9cad31876e0646677c4611
Florian Lepretre <florian.lprt@gmail.com>
2015-11-10 14:20:18 +0100

    Ajout des fichiers de base

---
+++ js/Score.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var photoScore = function () {
+	// private attributes and methods
+
+	// public methods
+}
+
+var pageScore = function () {
+	// private attributes and methods
+
+	// public methods
+}
+
+var albumScore = function () {
+	// private attributes and methods
+
+	// public methods
+
+};
